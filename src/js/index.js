8821a72416ebaf2f2cb4eb4321b28b3aefd7cc95
Hong Baojin <13809735212@163.com>
2018-12-05 18:11:30 +0800

    frist

---
+++ src/js/index.js
@@ -0,0 +1,23 @@
+// 头部右侧导航
+let navList = document.querySelectorAll('.nav > a');
+
+// 客户帮助 和 文章详情的 tab
+let helpList = document.querySelectorAll('.tab_header > .header_item');
+
+function active(activeList) {
+    for (let i = 0, len = activeList.length; i < len; i++) {
+        activeList[i].onclick = function () {
+            for (let j = 0; j < len; j++) {
+                activeList[j].classList.remove("active");
+            }
+            activeList[i].classList.add("active");
+        };
+    }
+}
+
+// 导航
+// active(navList);
+// 客户帮助 和 文章详情
+active(helpList);
+
+
